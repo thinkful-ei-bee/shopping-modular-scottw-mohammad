e6ca176ffbc59decd5873d8063b111601dd23de1
mak20th <mak20th@gmail.com>
2019-02-28 11:26:11 -0600

    add object shorthand in store.js

--- scripts/store.js
+++ scripts/store.js
@@ -3,16 +3,19 @@
 
 const store = (function () {
 
-  return {
-    items: [
-      { id: cuid(), name: 'apples', checked: false },
-      { id: cuid(), name: 'oranges', checked: false },
-      { id: cuid(), name: 'milk', checked: true },
-      { id: cuid(), name: 'bread', checked: false }
-    ],
-    hideCheckedItems: false,
-    searchTerm: ''
+  const items = [
+    { id: cuid(), name: 'apples', checked: false },
+    { id: cuid(), name: 'oranges', checked: false },
+    { id: cuid(), name: 'milk', checked: true },
+    { id: cuid(), name: 'bread', checked: false }
+  ];
+  const hideCheckedItems = false;
+  const searchTerm= '';  
+ 
+  return{
+    items,
+    hideCheckedItems,
+    searchTerm
   };
-
-
+  
 }() );
