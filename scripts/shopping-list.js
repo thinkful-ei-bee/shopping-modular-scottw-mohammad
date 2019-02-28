d029660c1b6c81da1569731b623a3d60f41a9edf
mak20th <mak20th@gmail.com>
2019-02-28 14:38:53 -0600

    finished nameSpacing 2

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,5 +1,5 @@
+/* eslint-disable no-undef */
 'use strict';
-/* global store, cuid */
 /*global $ */
 // eslint-disable-next-line no-unused-vars
 const shoppingList = (function(){
@@ -57,7 +57,14 @@ const shoppingList = (function(){
   
   
   function addItemToShoppingList(itemName) {
-    store.items.push({ id: cuid(), name: itemName, checked: false });
+    try{
+      Item.validateName(itemName);     
+      store.items.push( Item.create(itemName));
+      render();
+    }
+    catch(e){
+      console.log('Cannot add item: {error.message}');
+    }    
   }
   
   function handleNewItemSubmit() {
