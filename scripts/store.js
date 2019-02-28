6c3a31017a1b8da27239baac3df4def6d983e1f6
Scott Williams <scottcolemanwilliams@gmail.com>
2019-02-28 11:17:57 -0600

    added store.js

---
+++ scripts/store.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+'use strict';
+
+const store = (function () {
+
+  return {
+    items: [
+      { id: cuid(), name: 'apples', checked: false },
+      { id: cuid(), name: 'oranges', checked: false },
+      { id: cuid(), name: 'milk', checked: true },
+      { id: cuid(), name: 'bread', checked: false }
+    ],
+    hideCheckedItems: false,
+    searchTerm: ''
+  };
+
+
+}() );
